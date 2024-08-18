7ce0360de033891ac260cface0787049461ef8ed
Kssonety <152918484+Kssonety@users.noreply.github.com>
2024-08-18 18:46:32 +0100

    Add files via upload

---
+++ pixel_manager/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='pixel_manager',
+   version='0.1',
+   packages=['pixel_manager'],
+   description='A Package To Manage Pixels On Screen',
+   author='Kssonety',
+   author_email='aaron@innecco.com',
+   url='https://github.com/Kssonety/pixel_manager'
+)
