7fea3b296b66f1e9e741c9ef4776b82f1147396b
Kssonety <152918484+Kssonety@users.noreply.github.com>
2024-08-19 11:15:35 +0100

    Updated pixelmanager.py

--- pixelmanager.py
+++ pixelmanager.py
@@ -1,10 +1,20 @@
 from PIL import Image
 import numpy as np
 
-def MakeImageFromPixelRGB(PixelArray: list, ImgName: str, SaveImgAsFile: bool):
-    # Convert the pixels into an array using numpy
-    array = np.array(PixelArray, dtype=np.uint8)
+class MakeImgFromPixelRGB:
+    def createIMG(PixelArray: list, ImgName: str, SaveImgAsFile: bool):
+        # Convert the pixels into an array using numpy
+        array = np.array(PixelArray, dtype=np.uint8)
+        if SaveImgAsFile == True:
+            new_image = Image.fromarray(array)
+            new_image.save(ImgName)
+    def getPixelsFromIMG(ImageFile):
+        pass
 
-    if SaveImgAsFile == True:
-        new_image = Image.fromarray(array)
-        new_image.save(ImgName)
+
+MakeImgFromPixelRGB.createIMG([
+   [(54, 54, 54), (232, 23, 93), (71, 71, 71), (168, 167, 167)],
+   [(204, 82, 122), (54, 54, 54), (168, 167, 167), (232, 23, 93)],
+   [(71, 71, 71), (168, 167, 167), (54, 54, 54), (204, 82, 122)],
+   [(168, 167, 167), (204, 82, 122), (232, 23, 93), (54, 54, 54)]
+], "test.png", True)
