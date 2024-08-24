5d4dd846d9b40858b9f846ffcaf79197c15e5512
Kssonety <152918484+Kssonety@users.noreply.github.com>
2024-08-24 10:24:51 +0100

    Add files via upload

--- pixelmanager.py
+++ pixelmanager.py
@@ -2,9 +2,8 @@ from PIL import Image
 import numpy as np
 
 class MakeImgFromPixelRGB:
-    def createIMG(PixelArray: list, Height: int, Width: int, ImgName: str, SaveImgAsFile: bool):
-        # Convert the pixels into an array using numpy
-        array = np.array(PixelArray, dtype=np.uint8).reshape(Height, Width, 3)
+    def __init__(self, PixelArray: list, Height: int, Width: int, ImgName: str, SaveImgAsFile: bool):
+        array = np.array(PixelArray, dtype=np.uint8).reshape(Width, Height, 3)
         if SaveImgAsFile == True:
             new_image = Image.fromarray(array)
             new_image.save(ImgName)
@@ -15,3 +14,10 @@ class getPixelsFromIMG:
         im = Image.open(JpegPath)
         px = list(im.getdata())
         self.output = px
+
+class setPixelOnIMG:
+    def __init__(self, Pixel_X_Position, Pixel_Y_Position, ImgPath, ImgNAME: str, newRGBAtupleValue: tuple):
+        im = Image.open(ImgPath)
+        pix = im.load()
+        pix[Pixel_X_Position, Pixel_Y_Position] = newRGBAtupleValue
+        im.save(ImgNAME)
